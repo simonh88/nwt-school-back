5c013bac5943adb9ff293ea086fd0d27ed892519
Simon HAJEK <s.hajek@tadaweb.com>
2022-10-18 20:40:17 +0200

    feat(step-15): solution

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { HelloModule } from './hello/hello.module';
 import { PeopleModule } from './people/people.module';
+import * as Config from 'config';
 
 @Module({
-  imports: [HelloModule, PeopleModule],
+  imports: [
+    HelloModule,
+    PeopleModule,
+    MongooseModule.forRoot(Config.get<string>('mongodb.uri')),
+  ],
 })
 export class AppModule {}
