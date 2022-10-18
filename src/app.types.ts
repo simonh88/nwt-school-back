fbf9122fad424140bdcda222e88898729fdd7f0c
Simon HAJEK <s.hajek@tadaweb.com>
2022-10-18 20:34:23 +0200

    feat(step-14): solution

--- src/app.types.ts
+++ src/app.types.ts
@@ -2,3 +2,11 @@ export type AppConfig = {
   host: string;
   port: number;
 };
+
+export type SwaggerConfig = {
+  title: string;
+  description: string;
+  version: string;
+  tag: string;
+  path: string;
+};
--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,42 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import * as Config from 'config';
-import { AppConfig } from './app.types';
+import { AppConfig, SwaggerConfig } from './app.types';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PeopleModule } from './people/people.module';
 
-async function bootstrap(config: AppConfig) {
+async function bootstrap(config: AppConfig, swaggerConfig: SwaggerConfig) {
+  // create NestJS application
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+  // use global pipe validation
   await app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
+  // create swagger options
+  const options = new DocumentBuilder()
+    .setTitle(swaggerConfig.title)
+    .setDescription(swaggerConfig.description)
+    .setVersion(swaggerConfig.version)
+    .addTag(swaggerConfig.tag)
+    .build();
+
+  // create swagger document
+  const peopleDocument = SwaggerModule.createDocument(app, options, {
+    include: [PeopleModule],
+  });
+
+  // setup swagger module
+  SwaggerModule.setup(swaggerConfig.path, app, peopleDocument);
+
+  // launch server
   await app.listen(config.port, config.host);
   Logger.log(
     `Application served at http://${config.host}:${config.port}`,
@@ -26,4 +49,7 @@ async function bootstrap(config: AppConfig) {
   );
 }
 
-bootstrap(Config.get<AppConfig>('server'));
+bootstrap(
+  Config.get<AppConfig>('server'),
+  Config.get<SwaggerConfig>('swagger'),
+);
